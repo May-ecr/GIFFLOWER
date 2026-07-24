a2fb33023418a8060743f34eae5319f8cacceccd
Manuel Pastor <mangel2004xd@outlook.es>
2026-07-24 16:14:23 -0600

    Avance: Inclusión del servicio de Generación de Archivo .xlsx y ajuste de peticiones

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cotizacionRoutes from "./routes/cotizacionRoutes";
+import excelRoutes from "./routes/excelRoute";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/cotizacion", cotizacionRoutes);
+app.use("/cotizacion/excel", excelRoutes);
 
 export default app;
